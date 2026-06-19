6e4fd76163e31e94d918c9ace79b3b8c0547d4aa
Tom Bradley <t.ls.bradley38@gmail.com>
2026-06-19 07:01:32 +0100

    Added epic new main menu look

---
+++ UncappedSpireCode/UncappedUI/NMainMenuBgPatches.cs
@@ -0,0 +1,17 @@
+﻿using Godot;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Nodes.Screens.MainMenu;
+
+
+namespace UncappedSpire.UncappedSpireCode.UncappedUI;
+
+[HarmonyPatch(typeof(NMainMenuBg), "_Ready")]
+public class NMainMenuBgPatches
+{
+    [HarmonyPrefix]
+    public static void _Ready(NMainMenuBg __instance)
+    {
+        var light = __instance.GetNode<Node2D>("BgContainer/Bg");
+        light.Modulate = new Color(0.25f, 0.25f, 0.25f);
+    }
+}
