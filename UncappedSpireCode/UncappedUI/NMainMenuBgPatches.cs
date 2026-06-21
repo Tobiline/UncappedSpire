c67b67d173d200b50b6cb066d38811f237c75a90
Tom Bradley <t.ls.bradley38@gmail.com>
2026-06-22 00:37:26 +0100

    Replaced mod image and removed main menu bg changes

--- UncappedSpireCode/UncappedUI/NMainMenuBgPatches.cs
+++ UncappedSpireCode/UncappedUI/NMainMenuBgPatches.cs
@@ -1,19 +1,19 @@
-﻿using Godot;
-using HarmonyLib;
-using MegaCrit.Sts2.Core.Nodes.Screens.MainMenu;
-
-namespace UncappedSpire.UncappedSpireCode.UncappedUI;
-
-[HarmonyPatch(typeof(NMainMenuBg), "_Ready")]
-public class NMainMenuBgPatches
-{
-    [HarmonyPrefix]
-    public static void _Ready(NMainMenuBg __instance)
-    {
-        var bg = __instance.GetNode<Node2D>("BgContainer/Bg");
-        bg.Modulate = new Color(0.35f, 0.25f, 0.25f);
-        
-        var fg = __instance.GetNode<Node2D>("BgContainer/Fg");
-        fg.Modulate = new Color(0.75f, 0.5f, 0.5f);
-    }
-}
+﻿// using Godot;
+// using HarmonyLib;
+// using MegaCrit.Sts2.Core.Nodes.Screens.MainMenu;
+//
+// namespace UncappedSpire.UncappedSpireCode.UncappedUI;
+//
+// [HarmonyPatch(typeof(NMainMenuBg), "_Ready")]
+// public class NMainMenuBgPatches
+// {
+//     [HarmonyPrefix]
+//     public static void _Ready(NMainMenuBg __instance)
+//     {
+//         var bg = __instance.GetNode<Node2D>("BgContainer/Bg");
+//         bg.Modulate = new Color(0.35f, 0.25f, 0.25f);
+//         
+//         var fg = __instance.GetNode<Node2D>("BgContainer/Fg");
+//         fg.Modulate = new Color(0.75f, 0.5f, 0.5f);
+//     }
+// }
