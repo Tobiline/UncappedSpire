353b4e4a247d2694a108263b4b622d4bb1a1108d
Tom Bradley <t.ls.bradley38@gmail.com>
2026-06-23 17:20:24 +0100

    Fixed merchant bug where eggs would infinitely upgrade card rewards

---
+++ UncappedSpireCode/UncappedUpgrades/MerchantEggFixes/Patch_EggRelicHelper.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Helpers.Models;
+using MegaCrit.Sts2.Core.Models;
+
+namespace UncappedSpire.UncappedSpireCode.UncappedUpgrades.MerchantEggFixes;
+
+[HarmonyPatch(nameof(EggRelicHelper), nameof(EggRelicHelper.UpgradeValidCards))]
+public class Patch_EggRelicHelper
+{
+    [HarmonyPrefix]
+    public static void Prefix(ref List<CardCreationResult> cards, CardType cardType, RelicModel eggRelic)
+    {
+        var cardsThatHaveAlreadyBeenModified = cards.Where(card => !card.ModifyingRelics.Contains(eggRelic)).ToList();
+        cards = cardsThatHaveAlreadyBeenModified;
+    }
+}
