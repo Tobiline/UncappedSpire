d191d8ba8a82c41005f840e9243289b505e36ecd
Tom Bradley <t.ls.bradley38@gmail.com>
2026-06-25 10:22:48 +0100

    Defaulting the _enchantmentTabContainer to prevent NREs

--- UncappedSpireCode/UncappedEnchantments/UI/SpireField__enchantmentTabContainer.cs
+++ UncappedSpireCode/UncappedEnchantments/UI/SpireField__enchantmentTabContainer.cs
@@ -6,7 +6,7 @@ namespace UncappedSpire.UncappedSpireCode.UncappedEnchantments.UI;
 
 public class SpireField__enchantmentTabContainer
 {
-    public static readonly SpireField<NCard, NEnchantmentTabContainer> _enchantmentTabContainer = new(() => null);
+    public static readonly SpireField<NCard, NEnchantmentTabContainer> _enchantmentTabContainer = new(() => new());
     public static readonly SpireField<NCard, VBoxContainer> _enchantmentTabContainerLeft = new(() => new());
     // TODO: Make using this default, but optional
     public static readonly SpireField<NCard, VBoxContainer> _enchantmentTabContainerRight = new(() => new());
