f1ffb57f2d1c7bf1d17f79b7f4d9131870d022fd
Tom Bradley <t.ls.bradley38@gmail.com>
2026-06-28 22:20:43 +0100

    Added couple more fixes

---
+++ UncappedSpireCode/DebugTools/DebugUtil.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection.Emit;
+using HarmonyLib;
+
+namespace UncappedSpire.UncappedSpireCode.DebugTools;
+
+public static class DebugUtil
+{
+    public static void PrintDebug(this IEnumerable<CodeInstruction> instructions)
+    {
+        foreach (var instruction in instructions)
+        {
+            MainFile.Logger.Info($"{instruction.labels.FirstOrDefault().Id}: {instruction.opcode} {(instruction.operand is Label label ? label.Id : instruction.operand)}");
+        }
+    }
+}
---
+++ UncappedSpireCode/UncappedUpgrades/CardPatches/MaulTransformPatch.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Relics;
+using UncappedSpire.UncappedSpireCode.DebugTools;
+using UncappedSpire.UncappedSpireCode.UncappedUpgrades.CardModelPatches;
+
+namespace UncappedSpire.UncappedSpireCode.UncappedUpgrades.CardPatches;
+
+[HarmonyPatch(typeof(Claws), "CreateMaulFromOriginal")]
+public class MaulTransformPatch
+{
+    [HarmonyTranspiler]
+    static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        var code = new List<CodeInstruction>(instructions);
+        
+        var methodToFind = AccessTools.Method(typeof(CardModel), "UpgradeInternal");
+        var getCurrentUpgradeLevel = AccessTools.PropertyGetter(typeof(CardModel), nameof(CardModel.CurrentUpgradeLevel));
+        var safelySetCurrentUpgradeLevel = AccessTools.Method(typeof(CardModelExtensions),
+            nameof(CardModelExtensions.SafelySetCurrentUpgradeLevel));
+        
+        for (var i = 0; i < code.Count; i++)
+        {
+            var instruction = code[i];
+            if (instruction.opcode == OpCodes.Callvirt && instruction.operand is MethodInfo methodInfo && methodInfo == methodToFind)
+            {
+                var ldOriginal = code[i - 9];
+                var ldCardModel = code[i - 1].opcode;
+                
+                code.InsertRange(i + 3, [
+                    new CodeInstruction(ldOriginal),
+                    new CodeInstruction(OpCodes.Callvirt, getCurrentUpgradeLevel),
+                    new CodeInstruction(OpCodes.Ldc_I4_1),
+                    new CodeInstruction(OpCodes.Sub),
+                    new CodeInstruction(OpCodes.Call, safelySetCurrentUpgradeLevel),
+                    new CodeInstruction(ldCardModel)
+                ]);
+                
+                code.InsertRange(i - 1, [
+                    new CodeInstruction(ldCardModel),
+                    new CodeInstruction(ldOriginal),
+                    new CodeInstruction(OpCodes.Callvirt, getCurrentUpgradeLevel),
+                    new CodeInstruction(OpCodes.Ldc_I4_1),
+                    new CodeInstruction(OpCodes.Sub),
+                    new CodeInstruction(OpCodes.Call, safelySetCurrentUpgradeLevel)
+                ]);
+                
+                break;
+            }
+        }
+        
+        code.PrintDebug();
+    
+        return code;
+    }
+}
